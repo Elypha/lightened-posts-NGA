904c9fcf404f8712969bd58bf66d2abac8d914e3
Akira Ely <jamesintoast@gmail.com>
2018-10-21 14:27:21 +0800

    Update lightened-post.js
    UID is now pulled in a smoother way when this script is working with "NGA Auto Pager", which may help with potential lagging.

--- lightened-post.js
+++ lightened-post.js
@@ -2,12 +2,12 @@
 // @name            根据UID屏蔽主题帖及回复（NGA）
 // @name:en         Lightened-posts-NGA
 // @namespace       https://github.com/Elypha/lightened-posts
-// @version         0.4
+// @version         0.5
 // @description     通过指定UID，淡化显示其主题帖及回帖，达到护眼的效果。
 // @description:en  Alter unpleasing posts, according to posters' uid, into a lightened style.
 // @author          金光闪闪大萌德@NGA
 // @date            04/08/2018
-// @modified        06/08/2018
+// @modified        21/10/2018
 // @supportURL      https://github.com/Elypha/lightened-posts/issues
 // @match           bbs.nga.cn/thread.php*
 // @match           bbs.nga.cn/read.php*
@@ -20,10 +20,10 @@
 //localstorage.removeItem(uid_list)
 
 //PAGE_SWITCH
-var url = window.location.href
-var switch_url_thread = url.search(/thread\.php/)
-var switch_url_read = url.search(/\/read\.php/)
-var switch_url_nuke = url.search(/nuke\.php/)
+var url = window.location.href;
+var switch_url_thread = url.search(/thread\.php/);
+var switch_url_read = url.search(/\/read\.php/);
+var switch_url_nuke = url.search(/nuke\.php/);
 
 if (switch_url_nuke>0){
     //个人资料页面
@@ -31,7 +31,7 @@ if (switch_url_nuke>0){
     var button_block = document.createElement("button");
     button_block.innerHTML = "BLOCK";
 
-    var body_block = document.querySelector("#ucp_block")
+    var body_block = document.querySelector("#ucp_block");
     body_block.appendChild(button_block);
 
     button_block.addEventListener ("click", function() {
@@ -47,14 +47,14 @@ if (switch_url_nuke>0){
             var url = window.location.href;
             var uid = url.replace(/[^\d]/g,'');
 
-            var is_exist = read_list.search(uid)
+            var is_exist = read_list.search(uid);
             if (is_exist>=0){
                 alert("此用户已存在于黑名单中")
             }else{
                 read_list = uid + "|" + read_list;
-                read_list = read_list.replace(/\|\|/g, "|")
+                read_list = read_list.replace(/\|\|/g, "|");
                 localStorage.setItem("uid_list",read_list);
-                alert("成功加入黑名单")
+                alert("成功加入黑名单");
             }
         }
     });
@@ -62,7 +62,7 @@ if (switch_url_nuke>0){
     var button_block_remove = document.createElement("button");
     button_block_remove.innerHTML = "REMOVE";
 
-    var body_block_remove = document.querySelector("#ucp_block")
+    var body_block_remove = document.querySelector("#ucp_block");
     body_block_remove.appendChild(button_block_remove);
 
     button_block_remove.addEventListener ("click", function() {
@@ -78,13 +78,13 @@ if (switch_url_nuke>0){
             var url = window.location.href;
             var uid = url.replace(/[^\d]/g,'');
 
-            var is_exist = read_list.search(uid)
+            var is_exist = read_list.search(uid);
             if (is_exist>=0){
                 var uid_remove_RE = new RegExp(uid);
-                read_list = read_list.replace(uid_remove_RE, "")
-                read_list = read_list.replace(/\|\|/g, "|")
-                localStorage.setItem("uid_list",read_list)
-                alert("已从黑名单中移除")
+                read_list = read_list.replace(uid_remove_RE, "");
+                read_list = read_list.replace(/\|\|/g, "|");
+                localStorage.setItem("uid_list",read_list);
+                alert("已从黑名单中移除");
             }
         }
     });
@@ -92,30 +92,30 @@ if (switch_url_nuke>0){
     var button_block_input = document.createElement("button");
     button_block_input.innerHTML = "INPUT";
 
-    var body_block_input = document.querySelector("#ucp_block")
+    var body_block_input = document.querySelector("#ucp_block");
     body_block_input.appendChild(button_block_input);
 
     button_block_input.addEventListener ("click", function() {
-        var uid_input = prompt("输入uid，请参照下述格式\n单个uid:\n12340000\n多个uid:\n12340000|23450000|34560000","")
+        var uid_input = prompt("输入uid，请参照下述格式\n单个uid:\n12340000\n多个uid:\n12340000|23450000|34560000","");
         if(uid_input.length>1){
-            uid_input = uid_input.replace("｜", "|")
+            uid_input = uid_input.replace("｜", "|");
             var read_list = localStorage.getItem("uid_list");
             read_list = uid_input + "|" + read_list;
-            read_list = read_list.replace(/\|\|/g, "|")
+            read_list = read_list.replace(/\|\|/g, "|");
             localStorage.setItem("uid_list",read_list);
-            alert("成功加入黑名单")
+            alert("成功加入黑名单");
         }
     });
     //展出
     var button_block_view = document.createElement("button");
     button_block_view.innerHTML = "VIEW";
 
-    var body_block_view = document.querySelector("#ucp_block")
+    var body_block_view = document.querySelector("#ucp_block");
     body_block_view.appendChild(button_block_view);
 
     button_block_view.addEventListener ("click", function() {
         var read_list = localStorage.getItem("uid_list");
-        alert(read_list)
+        alert(read_list);
     });
 }
 
@@ -125,26 +125,28 @@ var uid_list_RE = new RegExp(uid_list);
 
 if (switch_url_thread>0){
     //主题帖列表页面
-    block_thread()
-    var table = document.getElementById('topicrows')
+    block_thread();
+    var height_thread = document.documentElement.scrollHeight;
     window.addEventListener('scroll', async () => {
-        clearTimeout()
-        setTimeout(500)
-        block_thread()
+        var height_thread_new = document.documentElement.scrollHeight;
+        if(height_thread_new - height_thread > 0){
+            block_thread();
+            height_thread = height_thread_new;
+        }
     })
 
     function block_thread() {
-        var rowslength = document.getElementById('topicrows').rows.length
-        var table = document.getElementById('topicrows')
-        var i
+        var rowslength = document.getElementById('topicrows').rows.length;
+        var table = document.getElementById('topicrows');
+        var i;
         for(i=0;i<rowslength;i++){
-            var str = document.getElementById('topicrows').rows[i].cells[2].innerHTML
-            var result = str.search(uid_list_RE)
+            var str = document.getElementById('topicrows').rows[i].cells[2].innerHTML;
+            var result = str.search(uid_list_RE);
             if (result > 0) {
-                var color_1 = document.getElementById('topicrows').rows[i].cells[2].querySelector("a[title]")
-                color_1.style.opacity=0.15
-                var color_2 = document.getElementById('topicrows').rows[i].querySelector("a.topic")
-                color_2.style.opacity=0.15
+                var color_1 = document.getElementById('topicrows').rows[i].cells[2].querySelector("a[title]");
+                color_1.style.opacity=0.15;
+                var color_2 = document.getElementById('topicrows').rows[i].querySelector("a.topic");
+                color_2.style.opacity=0.15;
             }
         }
     }
@@ -152,33 +154,33 @@ if (switch_url_thread>0){
 
 if (switch_url_read>0){
     //贴内页面
-    block_read()
-    var height_read = document.documentElement.scrollHeight
+    block_read();
+    var height_read = document.documentElement.scrollHeight;
     window.addEventListener('scroll', async () => {
-        var height_read_new = document.documentElement.scrollHeight
+        var height_read_new = document.documentElement.scrollHeight;
         if(height_read_new - height_read > 0){
-            block_read()
+            block_read();
+            height_read = height_read_new;
         }
     })
 
     function block_read() {
-        var i
-        var length = document.getElementById("m_posts_c").getElementsByClassName('postbox').length
-        var first_post_info = document.getElementById("m_posts_c").getElementsByClassName('posterinfo')[0]
-        var startpoint = first_post_info.id
-        var first_number = startpoint.replace(/[^\d.]/g,'')
-        var terminal = Number(length) + Number(first_number)
-
+        var length = document.getElementById("m_posts_c").getElementsByClassName('postbox').length;
+        var first_post_info = document.getElementById("m_posts_c").getElementsByClassName('posterinfo')[0];
+        var startpoint = first_post_info.id;
+        var first_number = startpoint.replace(/[^\d.]/g,'');
+        var terminal = Number(length) + Number(first_number);
+        var i;
         for(i=first_number;i<terminal;i++){
-            var info_number = 'postauthor' + i
-            var post_number = 'postcontent' + i
-            var post_avatar = 'posterinfo' + i
-            var str_url = document.getElementById(info_number)
-            var str = String(str_url)
-            var result = str.search(uid_list_RE)
+            var info_number = 'postauthor' + i;
+            var post_number = 'postcontent' + i;
+            var post_avatar = 'posterinfo' + i;
+            var str_url = document.getElementById(info_number);
+            var str = String(str_url);
+            var result = str.search(uid_list_RE);
             if (result > 0) {
-                document.getElementById(post_number).style.opacity=0.15
-                document.getElementById(post_avatar).style.opacity=0.15
+                document.getElementById(post_number).style.opacity=0.15;
+                document.getElementById(post_avatar).style.opacity=0.15;
             }
         }
     }
